8f4df31722ce36cc945e10c746e6c2bf40895752
Nate <nathan.randle@valpo.edu>
2019-06-07 13:40:24 -0500

    Create summary Table.py

---
+++ source/data analysis/summary Table.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  7 13:26:08 2019
+
+@author: nrandle
+"""
+
+import pandas as pd
+import numpy as np
+pd.set_option('display.max_rows', 100)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 180)
+
+import seaborn as sns
+
+df = pd.read_pickle("../../data/courses.pkl")
+print(df)
+
+
+"""
+summary table goal
+
+
+
+
+
+
+
+"""
