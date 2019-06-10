53314a2c672667b62f6404540dde6858e7def569
Nate <nathan.randle@valpo.edu>
2019-06-10 15:42:55 -0500

    combination heatmap

--- source/data analysis/summary Table.py
+++ source/data analysis/summary Table.py
@@ -9,18 +9,13 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from matplotlib import cm
 
 pd.set_option('display.max_rows', 100)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 180)
 
-import seaborn as sns
 
 df = pd.read_pickle("../../data/courses.pkl")
-
-#this grabs the 'within ds' values, removes the 0s for easy counting, counts all the collumns, 
-#transposes its from columns to rows (so each topic is a row), and turns the nans (which means the "agree"/"disagree" didnt show up) to 0s
 
 subQuestions = ["Within DS", 
                 "first course", 
@@ -28,30 +23,38 @@ subQuestions = ["Within DS",
                 "Learned in edu",
                 "Learned in job",
                 "Learned on own"]
+output = pd.DataFrame()
+
+#for each subheading
 for q in subQuestions:
     print("starting: " + q)
+    
+    #this grabs df, removes the 0s for easy counting, counts all the collumns, 
+    #transposes its from columns to rows (so each topic is a row), and turns the nans (which means the "agree"/"disagree" didnt show up) to 0s
+
     agreeCount = df.xs(q,level=1).replace(0,np.NaN).apply(pd.Series.value_counts).transpose().fillna(0)
     
-    
+    #create a count 
     agreeCount["n"] = agreeCount["Agree/Yes"] + agreeCount["Disagree/No"] 
     agreeCount["Agree/Yes"] = agreeCount["Agree/Yes"]/(agreeCount["Agree/Yes"]+agreeCount["Disagree/No"])
     agreeCount["Disagree/No"] = 1-agreeCount["Agree/Yes"]
+    agreeCount.columns = [q + " Agree/Yes", q + " Disagree/No", q + " n"]
+    output = pd.merge(output, agreeCount,how='outer', left_index = True, right_index = True)
     
     
-    
-    plt.figure(figsize=(6,40))
-    sns.set(font_scale=2)
-    
-    x = sns.heatmap(agreeCount.sort_values(["Agree/Yes","Disagree/No","n"],
-                                           ascending=False),
-                    annot=True, xticklabels=True, cbar=False,
-                    cmap=sns.color_palette("YlOrRd"),
-                    vmin = 0, vmax = 1,center=.5
-                    )
-    print("plotting: " + q)
-    plt.title("Topic %s" % q)
-    x = x.get_figure()
-    x.savefig("output%s.png" % q,dpi=200,bbox_inches="tight")
+plt.figure(figsize=(30,40))
+sns.set(font_scale=2)
+
+x = sns.heatmap(output,
+                annot=True, xticklabels=True, cbar=False,
+                cmap=sns.color_palette("YlOrRd"),
+                vmin = 0, vmax = 1,center=.5
+                )
+print("plotting: " + q)
+plt.title("Topic %s" % q)
+x = x.get_figure()
+x.savefig("output2.png" % q,dpi=200,bbox_inches="tight")
+
 
 """
 summary table goal
