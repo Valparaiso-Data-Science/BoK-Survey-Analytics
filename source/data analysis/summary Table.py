820690b2c89ffd89d7bf090bf91066ce35684399
Nate <nathan.randle@valpo.edu>
2019-06-10 13:09:17 -0500

    messing with heatmap coloring and settings

--- source/data analysis/summary Table.py
+++ source/data analysis/summary Table.py
@@ -18,16 +18,26 @@ pd.set_option('display.width', 180)
 import seaborn as sns
 
 df = pd.read_pickle("../../data/courses.pkl")
+
+#this grabs the 'within ds' values, removes the 0s for easy counting, counts all the collumns, 
+#transposes its from columns to rows (so each topic is a row), and turns the nans (which means the "agree"/"disagree" didnt show up) to 0s
 agreeCount = df.xs("Within DS",level=1).replace(0,np.NaN).apply(pd.Series.value_counts).transpose().fillna(0)
-agreeCount
+
+
+agreeCount["n"] = agreeCount["Agree/Yes"] + agreeCount["Disagree/No"] 
 agreeCount["Agree/Yes"] = agreeCount["Agree/Yes"]/(agreeCount["Agree/Yes"]+agreeCount["Disagree/No"])
 agreeCount["Disagree/No"] = 1-agreeCount["Agree/Yes"]
 
 
 
-plt.figure(figsize=(4,40))
+plt.figure(figsize=(6,40))
 sns.set(font_scale=2)
-x = sns.heatmap(agreeCount.sort_values(["Agree/Yes","Disagree/No"],ascending=False),annot=True,xticklabels=True,cbar=False,cmap=sns.color_palette("YlOrBr"),fmt=".1%")
+x = sns.heatmap(agreeCount.sort_values(["Agree/Yes","Disagree/No"],ascending=False),
+                annot=True,xticklabels=True,cbar=False,
+                cmap=sns.color_palette("YlOrRd"),
+                vmin = 0, vmax = 1,center=.5)
+
+
 x = x.get_figure()
 x.savefig("output.png",dpi=250,bbox_inches="tight")
 
