82603356b5785c1b4f56886a819ea938877c97fd
Nate <nathan.randle@valpo.edu>
2019-06-28 13:52:42 -0500

    finished column encoding

--- source/data analysis/demographics massaging.py
+++ source/data analysis/demographics massaging.py
@@ -12,7 +12,8 @@ pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 180)
 
 #importing the dataframe
-df = pd.read_csv("file:///H:/GitHub/BoK-Survey-Analytics/data/TRIPODS-X_June+19,+2019_09.54.csv",header=[0],skiprows=[2])
+df = pd.read_csv("file:///C:/Users/nrandle/Documents/GitHub/BoK-Survey-Analytics/data/TRIPODS-X_June+28,+2019_11.49.csv",
+                 header=[0],skiprows=[2])
 df = df.set_index(df["ResponseId"])
 
 qStart = df.columns.get_loc("UserLanguage")
@@ -20,15 +21,22 @@ qEnd = df.columns.get_loc("Q77.2")
 dfDemo = df.iloc[:,pd.np.r_[0:qStart, qEnd:len(df.columns)-2]]
 
 
-renameColumns = {"Q77.4_1":"White"}
-
 renameColumns = {'Q77.2':"Country", 'Q77.3':"State", 'Q77.4_1':"White", 'Q77.4_2':"Black or African American", 
                  'Q77.4_3':"American Indian or Alaska Native", 'Q77.4_4':"Asian",
-                 'Q77.4_5':"Native Hawaiian or Pacific Islander", 'Q77.4_6', 'Q77.4_6_TEXT', 'Q77.5', 'Q77.6', 'Q77.6_4_TEXT', 'Q78.1', 
-                 'Q78.2', 'Q78.2_5_TEXT', 'Q78.3', 'Q78.3_5_TEXT', 'Q78.4', 'Q78.5', 'Q78.5_5_TEXT',
-                 'Q78.6_1', 'Q78.6_2', 'Q78.6_3', 'Q78.6_4', 'Q78.6_5', 'Q78.6_6', 'Q78.6_7', 
-                 'Q78.6_8', 'Q78.6_8_TEXT', 'Q78.7', 'Q78.7_11_TEXT'}
-
+                 'Q77.4_5':"Native Hawaiian or Pacific Islander", 
+                 'Q77.4_6':"Other Race", 'Q77.4_6_TEXT':"Other Race Text", 
+                 'Q77.5':"Spanish, Hispanic, or Latino", 'Q77.6':"Pronoun", 
+                 'Q77.6_4_TEXT':"Pronoun Other", 
+                 'Q78.1':"Primary Role", 'Q78.2':"Area of Firm", 'Q78.2_5_TEXT':"Area of Firm other Text", 
+                 'Q78.3':"Position in Firm", 'Q78.3_5_TEXT':"Position in firm Other text",
+                 'Q78.4':"Years in Data Science",
+                 'Q78.5':"Academic position", 'Q78.5_5_TEXT':"Academic Position Other text",
+                 'Q78.6_1':"Math Dept", 'Q78.6_2':'Computer Science Dept', 'Q78.6_3':"Statistics Dept", 
+                 'Q78.6_4':"Data Science Dept", 'Q78.6_5':"Business Dept", 'Q78.6_6':"Engineering Dept",
+                 'Q78.6_7':"Other STEM Dept", 
+                 'Q78.6_8':"Other Dept", 'Q78.6_8_TEXT':"Other Dept Text", 
+                 'Q78.7':"School Classification", 'Q78.7_11_TEXT':"School Clasification Other"}
+dfDemo = dfDemo.drop(["Response ID","R_2ZDdGpzk73iWPXi"])
 dfDemo = dfDemo.rename(renameColumns,axis="columns")
 
 
